cfea336541ec5fab5936f8ee8c2d2aaaf924431b
Javier Alonso (Javinator9889) <dev@javinator9889.com>
2024-12-01 19:06:54 +0100

    Day 01: Add output for today's task

--- 2024/day01/main_test.go
+++ 2024/day01/main_test.go
@@ -22,11 +22,11 @@ func Test_part1(t *testing.T) {
 			input: example,
 			want:  11,
 		},
-		// {
-		// 	name:  "actual",
-		// 	input: input,
-		// 	want:  0,
-		// },
+		{
+			name:  "actual",
+			input: input,
+			want:  1222801,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -48,11 +48,11 @@ func Test_part2(t *testing.T) {
 			input: example,
 			want:  31,
 		},
-		// {
-		// 	name:  "actual",
-		// 	input: input,
-		// 	want:  0,
-		// },
+		{
+			name:  "actual",
+			input: input,
+			want:  22545250,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
