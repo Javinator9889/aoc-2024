100e7cc55d8ebee55da63241dbf309fc8bb4670f
Javier Alonso (Javinator9889) <dev@javinator9889.com>
2024-12-03 20:30:47 +0100

    Scripts: Add `slog` by default

--- scripts/skeleton/tmpls/main_test.go
+++ scripts/skeleton/tmpls/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/slog"
 	"testing"
 )
 
@@ -23,6 +24,7 @@ func Test_part1(t *testing.T) {
 		// 	want:  0,
 		// },
 	}
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := part1(tt.input); got != tt.want {
@@ -49,6 +51,7 @@ func Test_part2(t *testing.T) {
 		// 	want:  0,
 		// },
 	}
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := part2(tt.input); got != tt.want {
