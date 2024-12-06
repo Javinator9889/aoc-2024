73501b379a2d75f250159ba3c4393d6c9419335c
Javier Alonso (Javinator9889) <dev@javinator9889.com>
2024-12-06 12:56:42 +0100

    Day 06: We are back in the past!

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 	"strings"
 
-	"github.com/Javinator9889/aoc-2024/cast"
 	"github.com/Javinator9889/aoc-2024/util"
 )
 
@@ -44,20 +43,102 @@ func main() {
 	}
 }
 
-func part1(input string) int {
-	parsed := parseInput(input)
-	_ = parsed
+var UP = Dir{0, -1}
+var DOWN = Dir{0, 1}
+var LEFT = Dir{-1, 0}
+var RIGHT = Dir{1, 0}
 
-	return 0
+var dirTransform = map[rune]Dir{
+	'^': UP,
+	'v': DOWN,
+	'<': LEFT,
+	'>': RIGHT,
+}
+
+type Dir struct {
+	x, y int
+}
+
+type Position struct {
+	x, y     int
+	obstacle bool
+	visited  bool
+}
+
+type Guard struct {
+	x, y int
+	dir  Dir
+}
+
+type Map [][]Position
+
+func (m Map) outOfBounds(pos Position) bool {
+	return pos.x < 0 || pos.y < 0 || pos.x >= len(m[0]) || pos.y >= len(m)
+}
+
+func (g *Guard) moveForward() {
+	g.x += g.dir.x
+	g.y += g.dir.y
+}
+
+func part1(input string) (uniqueVisited int) {
+	mapp, guard := parseInput(input)
+	for {
+		slog.Debug("Guard", "guard", guard)
+		// First, mark out current position as visited
+		if !mapp[guard.y][guard.x].visited {
+			slog.Debug("Visited", "guard", guard, "position", mapp[guard.y][guard.x])
+			mapp[guard.y][guard.x].visited = true
+			uniqueVisited++
+		}
+		// The guard moves depending on the direction. There are two posibilites:
+		// 1. There is an obstacle in front of the guard.
+		// 2. There is no obstacle in front of the guard.
+		// If there is an obstacle, the guard will turn right. If there is no obstacle,
+		// the guard will move forward.
+		next := Position{x: guard.x + guard.dir.x, y: guard.y + guard.dir.y}
+		if mapp.outOfBounds(next) {
+			slog.Debug("Out of bounds", "next", next)
+			break // The guard has left the map
+		}
+		slog.Debug("Next", "next", mapp[next.y][next.x])
+		if mapp[next.y][next.x].obstacle {
+			// Turn right
+			guard.dir = Dir{-guard.dir.y, guard.dir.x}
+			slog.Debug("Turn right", "guard", guard)
+		} else {
+			// Move forward
+			guard.moveForward()
+			slog.Debug("Move forward", "guard", guard)
+		}
+	}
+
+	return
 }
 
 func part2(input string) int {
 	return 0
 }
 
-func parseInput(input string) (ans []int) {
-	for _, line := range strings.Split(input, "\n") {
-		ans = append(ans, cast.ToInt(line))
+func parseInput(input string) (mapp Map, guard Guard) {
+	mapp = make(Map, strings.Count(input, "\n")+1)
+	for i, line := range strings.Split(input, "\n") {
+		slog.Debug("Line", "i", i, "line", line)
+		mapp[i] = make([]Position, len(line))
+		for j, char := range line {
+			switch char {
+			case '#':
+				mapp[i][j] = Position{x: j, y: i, obstacle: true}
+			case '^', 'v', '<', '>':
+				guard = Guard{x: j, y: i, dir: dirTransform[char]}
+				fallthrough
+			case '.':
+				mapp[i][j] = Position{x: j, y: i, obstacle: false}
+			default:
+				msg := fmt.Sprintf(`invalid character in input: '%c' [%d, %d]`, char, i, j)
+				panic(msg)
+			}
+		}
 	}
-	return ans
+	return
 }
